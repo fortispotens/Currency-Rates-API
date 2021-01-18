6e6da540adac8d483d6927637f440d766415170f
Fortis <fortispotens@gmail.com>
2021-01-18 11:15:26 +0100

    setup fetch currency rates functionality

--- controllers/currencyRates.js
+++ controllers/currencyRates.js
@@ -6,9 +6,55 @@ const currencyRates = async (req, res) => {
 	const { rates } = fetched.data;
 	const { base, currency } = req.query;
 
-	console.log('>>>>rates', rates);
-	console.log('>>>>base', base);
-	console.log('>>>>currency', currency);
+	const fetchedCurrencies = currency.split(',');
+
+	fetchedCurrencies.push(base);
+
+	let fetchedCurrencyValues = [];
+	fetchedCurrencies.forEach((key) => {
+		const value = rates[key];
+		fetchedCurrencyValues.push(`${key} : ${value}`);
+	});
+
+	const splitfetchedCurrencyValues = (x) => {
+		const y = x.split(':');
+		return { [y[0].trim()]: y[1].trim() };
+	};
+
+	const fetchedFromStringValues = fetchedCurrencyValues.map(
+		splitfetchedCurrencyValues
+	);
+
+	const newBase = fetchedFromStringValues.pop();
+
+	const newConvertedBase = Object.values(newBase)[0];
+
+	const fetchRates = Object.assign({}, ...fetchedFromStringValues);
+
+	const entries = Object.entries(fetchRates);
+
+	const newRates = entries.map((entry) => {
+		return `${entry[0]} : ${Number(entry[1]) * Number(1 / newConvertedBase)}`;
+	});
+
+	const obj = newRates.map(splitfetchedCurrencyValues);
+
+	const convertedRates = Object.assign({}, ...obj);
+
+	Object.keys(convertedRates).forEach(function (el) {
+		convertedRates[el] = Number(Number(convertedRates[el]).toFixed(5));
+	});
+
+	console.log('>>>>>>', convertedRates);
+
+	const userResponse = {
+		base: base.toString(),
+		date: new Date().toLocaleDateString('en-GB'),
+		rates: convertedRates,
+	};
+	console.log('>>>>>>', userResponse);
+
+	res.send(userResponse);
 };
 
 export default currencyRates;
