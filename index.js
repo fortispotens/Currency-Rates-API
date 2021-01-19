4063191b4c102854e3b92110809af626a4877497
Fortis <fortispotens@gmail.com>
2021-01-19 05:27:41 +0100

    setup cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
 
 import currencyRatesRoutes from './routes/curencyRatesRoute.js';
 
